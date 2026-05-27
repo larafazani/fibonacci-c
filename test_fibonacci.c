5679dbcc0608814136783755d8a03b16706840cc
larafazani <laraegreja@gmail.com>
2026-05-27 19:15:38 -0300

    Create test_fibonacci.c

---
+++ test_fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
+#include "function_fibonacci.c"
+
+int main() 
+{
+    printf("TESTES AUTOMATIZADOS\n\n");
+
+    assert(calcular_fibonacci(0) == 0);
+    assert(calcular_fibonacci(1) == 1);
+    assert(calcular_fibonacci(6) == 8);
+    assert(calcular_fibonacci(9) == 34);
+
+    printf("Testes concluidos\n");
+    printf("Feito por: Lara Fazani);
+
+    return 0;
+}
